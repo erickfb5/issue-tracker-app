98431b90f533014f8022a82d1f3f0d8d6573a097
Erick Brandao <erickfb56@gmail.com>
2023-09-07 19:24:46 -0300

    modified:   public/js/main.js

--- public/js/main.js
+++ public/js/main.js
@@ -1,9 +1,11 @@
-document.addEventListener("DOMContentLoaded", () => {
-  const testForm = document.getElementById("testForm");
-  const testForm2 = document.getElementById("testForm2");
-  const testForm3 = document.getElementById("testForm3");
-
-  handleFormRequest(testForm, "POST");
-  handleFormRequest(testForm2, "PUT");
-  handleFormRequest(testForm3, "DELETE");
-});
+const setupFormHandlers = () => {
+    const testForm = document.getElementById("testForm");
+    const testForm2 = document.getElementById("testForm2");
+    const testForm3 = document.getElementById("testForm3");
+  
+    handleFormRequest(testForm, "POST");
+    handleFormRequest(testForm2, "PUT");
+    handleFormRequest(testForm3, "DELETE");
+  }
+  
+  document.addEventListener("DOMContentLoaded", setupFormHandlers);
